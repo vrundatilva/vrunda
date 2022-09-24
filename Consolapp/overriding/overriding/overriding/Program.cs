21631924b733f4c0b8c4e0c4c3c589e0c5a86f0e
vrunda <vrunda.tilva@gmail.com>
2022-09-24 10:46:10 +0530

    Add all repository

---
+++ Consolapp/overriding/overriding/overriding/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace overriding
+{
+    class bird//base class
+    {
+       public virtual void fly()
+        {
+            Console.WriteLine("Birds are Flying");
+        }
+    }
+    class Peacock : bird
+    {
+        public override void fly()
+        {
+            Console.WriteLine("Peacock are Flying");
+        }
+}
+    class program
+    {
+        static void Main(string[] args)
+        {
+            bird p = new Peacock();
+            p.fly();
+            Console.ReadLine();
+        }
+    }
+}
